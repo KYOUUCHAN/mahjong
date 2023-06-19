6a31b0773a88c3c4a9389ca3d88e93ac0e9f82e6
KYOUUCHAN <nk21137@tomakomai-ct.jp>
2023-06-20 00:58:37 +0900

    set stylesheet. require display_point.php.

--- mahjong/table.php
+++ mahjong/table.php
@@ -1,7 +1,7 @@
 <?php 
-    require 'radio.php' ;
+    require_once 'radio.php' ;
     require 'set_db.php' ;
-    require 'make_table.php'; 
+    require_once 'make_table.php'; 
 ?>
 
 <!DOCTYPE html>
@@ -10,22 +10,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="reset.css"> -->
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="index.css">
     <title>苫小牧高専麻雀成績</title>
 </head>
 <body>
-    <h3>苫小牧高専麻雀順位<h3>
-    <table border = "1">
-    <tr><th>順位</th><th>名前</th><th>点数</th></tr>
+    <h3>苫小牧高専麻雀順位</h3>
     <?php
-        for($i = 0 ; $i < count($count_table_name) ; $i++){
-            $rank += 1;
-            echo '<tr><th>'.$rank.'位</td>';
-            echo '<td>'.$count_table_name[$i].' プロ</td>';
-            echo '<td>'.$count_table_point[$i].' pt</td></tr>';
-        };
-        echo '</table>';
-        //for($i = 0 ; $i < )
+        require 'display_point.php';
+    ?>
+    <?php
         if($o == false){
             echo "error";
         }
