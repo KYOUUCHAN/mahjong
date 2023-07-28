a2435fcbd109e213fab82b8654ec0eace05c1547
KYOUUCHAN <nk21137@tomakomai-ct.jp>
2023-07-28 23:42:41 +0900

    text inserted to ""

--- mahjong/table.php
+++ mahjong/table.php
@@ -1,10 +1,10 @@
 <?php 
     require 'radio.php' ;
     require 'set_db.php' ;
-    if($_POST['number_of_people'] == YONMA){
+    if($_POST['number_of_people'] == "YONMA"){
         require_once 'make_table.php'; 
     }
-    else if($_POST['number_of_people'] == SANMA){
+    else if($_POST['number_of_people'] == "SANMA"){
         require_once 'make_table_sanma.php'; 
     }
     if($errorMessage == ""){
